4e8b6ef96f1212a113412e8e98323c53e217ed2d
aniket kumar <anikush0620@gmail.com>
2021-11-13 12:45:28 +0530

    preandpostincreament

---
+++ src/main/java/com/ank/preandpostincreament.java
@@ -0,0 +1,34 @@
+package com.ank;
+
+
+public class preandpostincreament {
+
+    public static void main(String[]args)
+    {
+        int x = 0;
+        int y = 5;
+
+        //x = x+1;
+        //x ++;
+        //x+=1;               // addition
+
+        //y -=3;
+        //y--;                  // substraction
+
+        //y/=3;                // division
+
+        //y*=2;               //multiplication
+
+        //y%=2;              // modulus
+
+        //pre post increament
+
+        y++;               //post increament
+
+        ++y;               //pre increament
+
+        System.out.println("y:" +y);
+
+
+    }
+}
