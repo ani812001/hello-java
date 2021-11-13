681875aa260b6b734540fe30aed8228ce4e67221
aniket kumar <anikush0620@gmail.com>
2021-11-13 23:26:30 +0530

    assiginingvariables

---
+++ src/main/java/com/ank/assiginingvariables.java
@@ -0,0 +1,17 @@
+package com.ank;
+
+public class assiginingvariables {
+
+    public static void main(String[] args)
+    {
+        int x,y,z;
+
+        //Assingment variables is done  from right to left.
+
+        x=y=z=2*5+4;
+
+        System.out.println("x:" +x);
+        System.out.println("y : " +y);
+        System.out.println("z :" +z);
+    }
+}
