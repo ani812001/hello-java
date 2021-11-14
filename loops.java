20a5cd76934597ffdccbd001fa8e4a0966695180
aniket kumar <77187170+ani812001@users.noreply.github.com>
2021-11-14 13:01:31 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,24 @@
+package com.ank;
+
+public class loops {
+
+    public static void main(String[] args) {
+        // get the square of the first  5 integers
+
+        //  System.out.println(1+ "Square = " +(1*1));
+        //  System.out.println(2+ "Square = " +(2*1));
+        //  System.out.println(3+ "Square = " +(3*1));
+        //  System.out.println(4+ "Square = " +(4*1));
+        //  System.out.println(5+ "Square = " +(5*1));
+
+
+        // using for loop
+        //for (initializing ;  contutation test> ; <update>) {
+        //the code you need to be reapeated
+        // }
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + "Squared =" + (i * i));
+        }
+    }
+}
